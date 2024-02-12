7edfac75aeef51d11ffada84b80ab114e65fce6b
zhan-star <yessenzholov@list.ru>
2024-02-12 16:36:58 +0600

    fixed api link

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
+import config from "./http/config.json";
 
+export const API_URL = config.API_URL;
 export default axios.create({
-  baseURL: "http://127.0.0.1:8080",
+  baseURL: API_URL,
 });
