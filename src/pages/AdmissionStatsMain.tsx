3a38a6646c018c0eeac234cce58b74e8275037c5
zhan-star <yessenzholov@list.ru>
2024-07-05 17:57:58 +0500

    temporary changes

--- src/pages/AdmissionStatsMain.tsx
+++ src/pages/AdmissionStatsMain.tsx
@@ -26,7 +26,7 @@ const AdmissionStatsMain: FC = () => {
     let current_year = d.getFullYear();
     const admissionStats = applicantStats.map((element) =>
         <tr key={element.specialization}>
-            <td id="table-divider-stats" style={{textAlign:'left'}}>{element.specialization}</td>
+            <td id="table-divider-stats-header" style={{textAlign:'left'}}>{element.specialization}</td>
             <td id="table-divider-stats">{element.sf1_sl1 }</td>
             <td id="table-divider-stats">{element.sf1_sl2 }</td>
             <td id="table-divider-stats">{element.sf3_sl1 }</td>
@@ -49,13 +49,23 @@ const AdmissionStatsMain: FC = () => {
             <td id="table-divider-stats">{element.sf30_sl2}</td>
             <td id="table-divider-stats">{element.sf31_sl1}</td>
             <td id="table-divider-stats">{element.sf31_sl2}</td>
-            <td id="table-divider-stats">{element.sf_fulltime}</td>
-            <td id="table-divider-stats">{element.sf_dl}</td>
-            <td id="table-divider-stats">{element.sf_overall}</td>
+            <td id="table-divider-stats" style={{fontWeight:'bold'}}>{element.sf_fulltime}</td>
+            <td id="table-divider-stats" style={{fontWeight:'bold'}}>{element.sf_dl}</td>
+            <td id="table-divider-stats" style={{fontWeight:'bold'}}>{element.sf_overall}</td>
             <td></td>
         </tr>
     );
-
+    //const lastelem = applicantStats[applicantStats.length];
+    //alert(applicantStats[applicantStats.length]);
+    //const miniheader1 = lastelem.sf1_sl1+lastelem.sf1_sl2;
+    //const miniheader2 = lastelem.sf5_sl1+lastelem.sf5_sl2;
+    //const miniheader3 = lastelem.sf24_sl1+lastelem.sf24_sl2;
+    const admissionStatsMini = applicantStats.map((element) =>
+    <div key={element.specialization}>
+        На базе школы: {element.sf1_sl1+element.sf1_sl2}<br/>
+        На базе колледжа: {element.sf5_sl1+element.sf5_sl2}<br/>
+        На базе высшего: {element.sf24_sl1+element.sf24_sl2}<br/>{element.specialization}</div>
+    );
     return (
         <div>
             {(() => {
@@ -67,50 +77,55 @@ const AdmissionStatsMain: FC = () => {
                         <br /><br /><br /><br />
                         {/* <h3>Добро пожаловать, {user.lastname + ' ' + user.name + ' ' + user.middlename}</h3><br /> */}
                         <h2 style={{ marginLeft: '-24%'}}>Статистика по абитуриентам ({current_year}-{current_year+1} учебный год)</h2>
+                        {/* <br/>
+                        <div id="table-divider-stats-header" style={{marginLeft: '-24%',textAlign:'left',paddingLeft:'4%',height:'100%',width:'25%'}}><br/>{admissionStatsMini}<br/></div>
+                        <br/> */}
                         <table id='opaqueTable' style={{ fontSize:'8pt', marginLeft: '-26%', paddingLeft: '15px', width: '107%' }}>
-                            <tbody><tr>
-                                <th style={{textAlign:'center'}}><br/><br/>ОП</th>
+                            <tbody>
+                                <tr><br/></tr>
+                                <tr>
+                                <th rowSpan={2} id="table-divider-stats-header" style={{textAlign:'center'}}><br/><br/>ОП<br/><br/></th>
                                 {/* 1,3,4,5,8,15,17,21,23,24,29,30,31 */}
-                                <th colSpan={2} id="table-divider-stats"><br />&nbsp;Очное (ср.школы)<br />&nbsp;</th>
-                                <th colSpan={2} id="table-divider-stats"><br />&nbsp;ДОТ на базе высшего<br />&nbsp;</th>
-                                <th colSpan={2} id="table-divider-stats"><br />&nbsp;Сокращенный ДОТ (очная) на базе колледжа<br />&nbsp;</th>
-                                <th colSpan={2} id="table-divider-stats"><br />&nbsp;Очное на базе Колледжа<br />&nbsp;</th>
-                                <th colSpan={2} id="table-divider-stats"><br />&nbsp;ДОТ (очное) 3 г.<br />&nbsp;</th>
-                                <th colSpan={2} id="table-divider-stats"><br />&nbsp;Научно-педагогическое<br />&nbsp;</th>
-                                <th colSpan={2} id="table-divider-stats"><br />&nbsp;Профильное<br />&nbsp;</th>
-                                <th colSpan={2} id="table-divider-stats"><br />&nbsp;Очное на базе Высшего<br />&nbsp;</th>
-                                <th colSpan={2} id="table-divider-stats"><br />&nbsp;Очное на базе Колледжа (2 г. обуч)<br />&nbsp;</th>
-                                <th colSpan={2} id="table-divider-stats"><br />&nbsp;ДОТ (очное) 2 г. на базе колледжа<br />&nbsp;</th>
-                                <th colSpan={2} id="table-divider-stats"><br />&nbsp;Очное (ср.школы) 3 г.<br />&nbsp;</th>
-                                <th colSpan={2} id="table-divider-stats">Итого</th>
-                                <th rowSpan={2} id="table-divider-stats">Всего</th>
+                                <th colSpan={2} id="table-divider-stats-header"><br />&nbsp;Очное (ср.школы)<br />&nbsp;</th>
+                                <th colSpan={2} id="table-divider-stats-header"><br />&nbsp;ДОТ на базе высшего<br />&nbsp;</th>
+                                <th colSpan={2} id="table-divider-stats-header"><br />&nbsp;Сокращенный ДОТ (очная) на базе колледжа<br />&nbsp;</th>
+                                <th colSpan={2} id="table-divider-stats-header"><br />&nbsp;Очное на базе Колледжа<br />&nbsp;</th>
+                                <th colSpan={2} id="table-divider-stats-header"><br />&nbsp;ДОТ (очное) 3 г.<br />&nbsp;</th>
+                                <th colSpan={2} id="table-divider-stats-header"><br />&nbsp;Научно-педагогическое<br />&nbsp;</th>
+                                <th colSpan={2} id="table-divider-stats-header"><br />&nbsp;Профильное<br />&nbsp;</th>
+                                <th colSpan={2} id="table-divider-stats-header"><br />&nbsp;Очное на базе Высшего<br />&nbsp;</th>
+                                <th colSpan={2} id="table-divider-stats-header"><br />&nbsp;Очное на базе Колледжа (2 г. обуч)<br />&nbsp;</th>
+                                <th colSpan={2} id="table-divider-stats-header"><br />&nbsp;ДОТ (очное) 2 г. на базе колледжа<br />&nbsp;</th>
+                                <th colSpan={2} id="table-divider-stats-header"><br />&nbsp;Очное (ср.школы) 3 г.<br />&nbsp;</th>
+                                <th colSpan={2} id="table-divider-stats-header">&nbsp;&nbsp;Итого</th>
+                                <th rowSpan={2} id="table-divider-stats-header">&nbsp;&nbsp;Всего</th>
                             </tr>
                             <tr>
-                                <th></th>
-                                <th id="table-divider-stats">(р.o.)</th>   
-                                <th id="table-divider-stats">(к.o.)</th>
-                                <th id="table-divider-stats">(р.o.)</th>   
-                                <th id="table-divider-stats">(к.o.)</th>
-                                <th id="table-divider-stats">(р.o.)</th>   
-                                <th id="table-divider-stats">(к.o.)</th>
-                                <th id="table-divider-stats">(р.o.)</th>   
-                                <th id="table-divider-stats">(к.o.)</th>
-                                <th id="table-divider-stats">(р.o.)</th>   
-                                <th id="table-divider-stats">(к.o.)</th>
-                                <th id="table-divider-stats">(р.o.)</th>   
-                                <th id="table-divider-stats">(к.o.)</th>
-                                <th id="table-divider-stats">(р.o.)</th>   
-                                <th id="table-divider-stats">(к.o.)</th>
-                                <th id="table-divider-stats">(р.o.)</th>   
-                                <th id="table-divider-stats">(к.o.)</th>
-                                <th id="table-divider-stats">(р.o.)</th>   
-                                <th id="table-divider-stats">(к.o.)</th>
-                                <th id="table-divider-stats">(р.o.)</th>   
-                                <th id="table-divider-stats">(к.o.)</th>
-                                <th id="table-divider-stats">(р.o.)</th>   
-                                <th id="table-divider-stats">(к.o.)</th>
-                                <th id="table-divider-stats">(Очная)</th>   
-                                <th id="table-divider-stats">(ДОТ)</th>
+                                
+                                <th id="table-divider-stats-header">(р.o.)</th>   
+                                <th id="table-divider-stats-header">(к.o.)</th>
+                                <th id="table-divider-stats-header">(р.o.)</th>   
+                                <th id="table-divider-stats-header">(к.o.)</th>
+                                <th id="table-divider-stats-header">(р.o.)</th>   
+                                <th id="table-divider-stats-header">(к.o.)</th>
+                                <th id="table-divider-stats-header">(р.o.)</th>   
+                                <th id="table-divider-stats-header">(к.o.)</th>
+                                <th id="table-divider-stats-header">(р.o.)</th>   
+                                <th id="table-divider-stats-header">(к.o.)</th>
+                                <th id="table-divider-stats-header">(р.o.)</th>   
+                                <th id="table-divider-stats-header">(к.o.)</th>
+                                <th id="table-divider-stats-header">(р.o.)</th>   
+                                <th id="table-divider-stats-header">(к.o.)</th>
+                                <th id="table-divider-stats-header">(р.o.)</th>   
+                                <th id="table-divider-stats-header">(к.o.)</th>
+                                <th id="table-divider-stats-header">(р.o.)</th>   
+                                <th id="table-divider-stats-header">(к.o.)</th>
+                                <th id="table-divider-stats-header">(р.o.)</th>   
+                                <th id="table-divider-stats-header">(к.o.)</th>
+                                <th id="table-divider-stats-header">(р.o.)</th>   
+                                <th id="table-divider-stats-header">(к.o.)</th>
+                                <th id="table-divider-stats-header">(Очная)</th>   
+                                <th id="table-divider-stats-header">(ДОТ)</th>
                             </tr>
                             {admissionStats}
                             <tr>
