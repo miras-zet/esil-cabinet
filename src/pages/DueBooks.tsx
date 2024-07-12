4583194d506df1dd8e40d9e5b6d9963da821a0f8
zhan-star <yessenzholov@list.ru>
2024-07-12 11:00:02 +0500

    due books filters added

--- src/pages/DueBooks.tsx
+++ src/pages/DueBooks.tsx
@@ -8,14 +8,18 @@ import { FaTrashAlt } from 'react-icons/fa';
 import { TiArrowBack } from 'react-icons/ti';
 import IBookTransfer from '../models/IBookTransfer';
 import moment from 'moment';
+import { ImCross } from 'react-icons/im';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
 
 const DueBooks: FC = () => {
     // const navigate = useNavigate();
     const [dueData, setDueData] = useState<Array<IBookTransfer>>([]);
-    let [margin, setMargin] = useState<string>('-15%');
+    let [fiofilter, setFioFilter] = useState<string>('');
+    let [booknamefilter, setBookNameFilter] = useState<string>('');
+    //let [margin, setMargin] = useState<string>('-15%');
     useEffect(() => {
-        setMargin('-15%');
-        if(window.innerWidth<940) setMargin('0%');
+        // setMargin('-15%');
+        // if(window.innerWidth<940) setMargin('0%');
         // const user = JSON.parse(localStorage.getItem('data'));
         BookService.getDueBooks().then((response) => {
             setDueData(response.data);
@@ -25,73 +29,94 @@ const DueBooks: FC = () => {
         });
 
     }, [])
-    // useEffect(()=>{
-    //   setModal(modals)
-    // },[])
+    const clearFilter = async (id: number) => {
+        switch (id) {
+            case 1: {
+                (document.getElementById("fioFilter") as HTMLInputElement).value = '';
+                setFioFilter('');
+            }
+                break;
+            case 2: {
+                (document.getElementById("bookNameFilter") as HTMLInputElement).value = '';
+                setBookNameFilter('');
+            }
+                break;
+        }
+    }
     //alert(date);
     //const d = new Date();
     //let current_year = d.getFullYear();
-    const deleteBook = async (id: number, name:string) => {
+    const deleteBook = async (id: number, name: string) => {
         if (confirm(`Вы уверены, что хотите удалить закрепление для студента ${name}?`)) {
-          BookService.resolveBookTransfer(id)
-            .then(() => {
-              return BookService.getDueBooks();
-            })
-            .then((due) => {
-                setDueData(due.data);
-            })
-            .catch((err) => {
-              if (err.response && err.response.data && err.response.data.message) {
-                alert(err.response.data.message);
-              } else {
-                alert("Ошибка удаления");
-              }
-            });
+            BookService.resolveBookTransfer(id)
+                .then(() => {
+                    return BookService.getDueBooks();
+                })
+                .then((due) => {
+                    setDueData(due.data);
+                })
+                .catch((err) => {
+                    if (err.response && err.response.data && err.response.data.message) {
+                        alert(err.response.data.message);
+                    } else {
+                        alert("Ошибка удаления");
+                    }
+                });
         }
     };
-    const booklist = dueData.map((element) =>
-        <tr key={element.id}>
-            <td id="table-divider-stats">{element.fio}</td>
-            <td id="table-divider-stats">{element.bookname}</td>
-            <td id="table-divider-stats">{moment(element.DateCreated).format("DD.MM.YYYY HH:mm")}</td>
-            <td id="table-divider-stats"><button className="redbutton" onClick={() => deleteBook(element.id, element.fio)}><FaTrashAlt /></button></td>
-        </tr>
+    const booklist = dueData.map((element) => {
+        let clearedfio = '';
+        let clearedbookname = '';
+        if (element.fio != null) clearedfio = element.fio;
+        if (element.bookname != null) clearedbookname = element.bookname;
+        if (clearedfio.toLowerCase().includes(fiofilter.toLowerCase())
+            && clearedbookname.toLowerCase().includes(booknamefilter.toLowerCase())) return <tr key={element.id}>
+                <td id="table-divider-stats">{element.fio}</td>
+                <td id="table-divider-stats">{element.bookname}</td>
+                <td id="table-divider-stats">{moment(element.DateCreated).format("DD.MM.YYYY HH:mm")}</td>
+                <td id="table-divider-stats"><button className="redbutton" onClick={() => deleteBook(element.id, element.fio)}><FaTrashAlt /></button></td>
+            </tr>
+    }
+
     );
-    
+
     return (
         <div>
             {(() => {
                 //const role = localStorage.getItem('role');
                 //const user = JSON.parse(localStorage.getItem('data'));
-                
-                    return <div style={{ textAlign: 'left', width: '900px', marginTop: '10%' }}>
-                        <KPINavbar />
-                        <br /><br /><br />
-                        {/* <h3>Добро пожаловать, {user.lastname + ' ' + user.name + ' ' + user.middlename}</h3><br /> */}
-                        <Link to={"/"}><button className='backbutton'><TiArrowBack style={{ verticalAlign: 'middle', marginTop: '-4px' }} /> Вернуться назад</button></Link> <br /><br />
-                        <br />
-                        <h2>Список должников по книгам</h2>
-                        <br/>
-                        
-                        {dueData.length>0?<table id='opaqueTable' style={{ fontSize:'12pt', paddingLeft: '15px', width: '107%' }}>
-                            <tbody>
-                                <tr><br/></tr>
-                                <tr>
+
+                return <div style={{ textAlign: 'left', width: '900px', marginTop: '10%' }}>
+                    <KPINavbar />
+                    <br /><br /><br />
+                    {/* <h3>Добро пожаловать, {user.lastname + ' ' + user.name + ' ' + user.middlename}</h3><br /> */}
+                    <Link to={"/"}><button className='backbutton'><TiArrowBack style={{ verticalAlign: 'middle', marginTop: '-4px' }} /> Вернуться назад</button></Link> <br /><br />
+                    <br />
+                    <h2>Список должников по книгам</h2>
+                    <br />
+
+                    {dueData.length > 0 ? <table id='opaqueTable' style={{ fontSize: '12pt', paddingLeft: '15px', width: '107%' }}>
+                        <tbody>
+                            <tr><br /></tr>
+                            <tr>
                                 <th id="table-divider-stats-header"><br />&nbsp;ФИО студента<br />&nbsp;</th>
                                 <th id="table-divider-stats-header"><br />&nbsp;Название книги<br />&nbsp;</th>
                                 <th id="table-divider-stats-header"><br />&nbsp;Дата выдачи<br />&nbsp;</th>
                                 <th id="table-divider-stats-header"><br />&nbsp;Действия<br />&nbsp;</th>
                                 <th>&nbsp;&nbsp;</th>
                             </tr>
-                            
+                            <tr>
+                                <th><div className='btn' style={{ paddingLeft: '15%', whiteSpace: 'nowrap', backgroundColor: 'lightgrey', color: 'black' }}><FaMagnifyingGlass />&nbsp;&nbsp;<input type="text" id="fioFilter" style={{ backgroundColor: 'lightgrey', color: 'black' }} onChange={() => setFioFilter((document.getElementById("fioFilter") as HTMLInputElement).value)}></input>{fiofilter.length > 0 ? <b onClick={() => clearFilter(1)} style={{ fontSize: '10pt', fontWeight: 'bold', marginLeft: '-16px', marginTop: '-5px' }}><ImCross /></b> : ''}</div></th>
+                                <th><div className='btn' style={{ paddingLeft: '15%', whiteSpace: 'nowrap', backgroundColor: 'lightgrey', color: 'black' }}><FaMagnifyingGlass />&nbsp;&nbsp;<input type="text" id="bookNameFilter" style={{ backgroundColor: 'lightgrey', color: 'black' }} onChange={() => setBookNameFilter((document.getElementById("bookNameFilter") as HTMLInputElement).value)}></input>{booknamefilter.length > 0 ? <b onClick={() => clearFilter(2)} style={{ fontSize: '10pt', fontWeight: 'bold', marginLeft: '-16px', marginTop: '-5px' }}><ImCross /></b> : ''}</div></th>
+                            </tr>
                             {booklist}
                             <tr>
-                                <td><br/></td>
+                                <td><br /></td>
                             </tr>
-                            </tbody>
-                        </table>:<h3>Должников нет</h3>}
-                    </div>   
-                
+                        </tbody>
+                    </table> : <h3>Должников нет</h3>}
+                </div>
+
             })()}
 
         </div>
