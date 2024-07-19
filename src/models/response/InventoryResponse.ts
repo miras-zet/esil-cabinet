951e5027f8562e2c29dd211f81915886c6460f4a
zhan-star <yessenzholov@list.ru>
2024-07-19 12:10:03 +0500

    inventory data display fixes

--- src/models/response/InventoryResponse.ts
+++ src/models/response/InventoryResponse.ts
@@ -7,6 +7,9 @@ export interface InventoryResponse {
     certificate_serial: string,
     certificate_number: string,
     certificate_date: string,
+    alternative_certificate_serial: string,
+    alternative_certificate_number: string,
+    alternative_certificate_date: string,
     exam_cert:string,
     exam_cert_date: Date
 }
