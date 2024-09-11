e5deb8fff3423ba1def284c3217b5b11b491b49e
zhan-star <yessenzholov@list.ru>
2024-09-11 17:09:54 +0500

    forgot to edit version

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -25,7 +25,7 @@ import IBookCart from '../models/IBookCart';
 // const api_url=process.env.REACT_APP_API_URL;
 
 const HomePage: FC = () => {
-  const currentVersion = '1.0.0';
+  const currentVersion = '1.0.1';
   const cachedVersion = localStorage.getItem('appVersion');
 
   if (cachedVersion !== currentVersion) {
