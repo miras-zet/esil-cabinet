3547af1081fa26d6ea865e24508a371caf624a6c
zhan-star <yessenzholov@list.ru>
2024-09-20 09:28:49 +0500

    ebooks test

--- src/pages/ReadEBook.tsx
+++ src/pages/ReadEBook.tsx
@@ -1,31 +1,26 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect } from 'react'
 import { observer } from 'mobx-react-lite';
 import '../App.css';
-import { pdfjs } from 'react-pdf';
-import { Document, Page } from 'react-pdf';
+import { EmbedPDF } from "@simplepdf/react-embed-pdf";
 import config from "../http/config.json";
 
 export const path = config.eBookPath;
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.mjs',
-    import.meta.url,
-).toString();
 
 const ReadEBook: FC = () => {
-    const [numPages, setNumPages] = useState<number>(1);
-    const [pageNumber, setPageNumber] = useState<number>(1);
+    //const [numPages, setNumPages] = useState<number>(1);
+    //const [pageNumber, setPageNumber] = useState<number>(1);
 
     //const navigate = useNavigate();
-    let fileUrl = path+'eLibraryBooks/'+localStorage.getItem('pdfURL');
+    let fileUrl = path + 'eLibraryBooks/' + localStorage.getItem('pdfURL');
     //fileUrl = 'http://10.0.1.22/CSP/euniversity/img/eLibraryBooks/Qatarlar.Eselik%20intervaldar.%20Oris%20teoriasinin%20elementterie.pdf';
     //fileUrl = 'http://arxiv.org/pdf/2407.15633';
     useEffect(() => {
 
     }, [])
-    function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-        setNumPages(numPages);
-    }
+    // function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    //     setNumPages(numPages);
+    // }
     // const return = () => {
     //     
     //     navigate(`/ebooks`);
@@ -34,14 +29,15 @@ const ReadEBook: FC = () => {
         <div>
             {(() => {
                 return <div>
-                    <button onClick={()=>pageNumber>1?setPageNumber(pageNumber-1):setPageNumber(1)}>Пред. страница</button>&nbsp;<button onClick={()=>pageNumber<numPages?setPageNumber(pageNumber+1):setPageNumber(numPages)}>След. страница</button>
-                    <p>
+                    {/* <button onClick={() => pageNumber > 1 ? setPageNumber(pageNumber - 1) : setPageNumber(1)}>Пред. страница</button>&nbsp;<button onClick={() => pageNumber < numPages ? setPageNumber(pageNumber + 1) : setPageNumber(numPages)}>След. страница</button> */}
+                    {/* <p>
                         Страница {pageNumber} из {numPages}
-                    </p>
-                    <Document file={fileUrl} onLoadSuccess={onDocumentLoadSuccess}>
-                        <Page pageNumber={pageNumber} />
-                    </Document>
-                    
+                    </p> */}
+                    <EmbedPDF
+                        mode="inline"
+                        style={{ width: 900, height: 800 }}
+                        documentURL={fileUrl}
+                    />
                 </div>
 
             })()}
