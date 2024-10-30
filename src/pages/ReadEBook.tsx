fecebeb5940b12867cd387167e92ceaef332fc18
zhan-star <yessenzholov@list.ru>
2024-10-30 13:07:04 +0500

    readebook tests

--- src/pages/ReadEBook.tsx
+++ src/pages/ReadEBook.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect } from 'react'
 import { observer } from 'mobx-react-lite';
 import '../PDF.css';
 import config from "../http/config.json";
+import { API_URL } from '../http';
 
 export const path = config.eBookPath;
 
@@ -45,7 +46,7 @@ const ReadEBook: FC = () => {
                         src={'http://10.0.1.22/CSP/euniversity/img/eLibraryBooks/'+localStorage.getItem('pdfURL') + "#toolbar=0"} 
                     /> */}
                      <iframe 
-    src={'http://127.0.0.1:8080/view/'+localStorage.getItem('pdfURL')} 
+    src={API_URL+'/view/'+localStorage.getItem('pdfURL')} 
     style={{border: "none", width:'100%', height:'105%',position:'absolute', top:-40,left:0}}>
   </iframe>
                 </div>
